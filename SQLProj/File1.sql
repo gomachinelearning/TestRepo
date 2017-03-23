c70f5fd9e771743725846e09c75fee63ff49ba5c
ojns <ocesteban@gmail.com>
2017-03-23 18:08:59 -0400

    Update File1.sql

--- SQLProj/File1.sql
+++ SQLProj/File1.sql
@@ -1,9 +1,1 @@
-change1
-change2 Stash
-change3 from local
-change4 from local changedefaults branc
-change4 from remote
-merged
-change5 from notepad++
-change6 from notepad++
-change7 from notepad++
+change1 from remote
