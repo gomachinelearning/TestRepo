653058f288f2458c10bfdac6a808a8c48a057f68
orly.esteban <orly.esteban@usac-cprod247.usac.loc>
2017-03-23 17:25:07 -0400

    cm merged

--- SQLProj/File1.sql
+++ SQLProj/File1.sql
@@ -3,6 +3,7 @@ change2 Stash
 change3 from local
 <<<<<<< HEAD
 change4 from local changedefaults branc
-=======
+======= merged
 change4 from remote
 >>>>>>> branch 'master' of https://github.com/ojns/TestRepo.git
+merged
